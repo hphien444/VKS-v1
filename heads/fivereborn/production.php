8a2cb1fbecfeeab32412321157fe9b2162b56e2c
VKS Builder <builder@local>
2026-05-28 18:45:24 +0700

    Keep launcher UI open while game window starts

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000016');
-header('x-amz-meta-bootstrap-size: 4071292');
-header('x-amz-meta-bootstrap-object: 2ed4a3cb4fbf16f4c0b706d50f6d92efaed802a5550db594c45fa9d181b3c753');
-header('x-amz-meta-branch-version: 1779959246');
+header('x-amz-meta-bootstrap-version: 1000017');
+header('x-amz-meta-bootstrap-size: 4071784');
+header('x-amz-meta-bootstrap-object: bcec51b1efd54e8569e28ad754194b00c488510c6e4d22556d62099bd7b751f3');
+header('x-amz-meta-branch-version: 1779968572');
 header('x-amz-meta-branch-manifest: 01912d85a6505e89d035514d4077146e4a51fddce4c791bdb8473be00301bfa6');
 echo "OK";
 ?>
