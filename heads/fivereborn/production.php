ff64a3d4b18c204f5e47b7b656100494d72be0a6
hphien444 <hphien444@users.noreply.github.com>
2026-05-28 10:38:50 +0700

    Publish VKS v1 update

---
+++ heads/fivereborn/production.php
@@ -0,0 +1,8 @@
+<?php
+header('x-amz-meta-bootstrap-version: 1000016');
+header('x-amz-meta-bootstrap-size: 4070396');
+header('x-amz-meta-bootstrap-object: fde699f4662c16197ad5b8aa7fc439a03b26bfe2658223a83d60ca79f11100cc');
+header('x-amz-meta-branch-version: 1779939116');
+header('x-amz-meta-branch-manifest: 2621178e5070591f1c68d52424a68aca60649eba0c3989a21ff7bb70b2706a04');
+echo "OK";
+?>
