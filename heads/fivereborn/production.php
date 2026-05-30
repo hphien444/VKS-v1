1df097e56d002c6760afdb4035b2f794625e9767
hphien444 <hphien444@users.noreply.github.com>
2026-05-30 23:58:40 +0700

    Fix VKS startup loading timeout 1000019

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000018');
-header('x-amz-meta-bootstrap-size: 4071024');
-header('x-amz-meta-bootstrap-object: 7544e6983bca3901b8f694356ec9f753f79743ed540cf69aa224cac22d6aa610');
-header('x-amz-meta-branch-version: 1780159543');
+header('x-amz-meta-bootstrap-version: 1000019');
+header('x-amz-meta-bootstrap-size: 4071684');
+header('x-amz-meta-bootstrap-object: e392b7a03faac5a3933c5c34c5ce3a55f897f056fe79d889e0e3f8102ee43a11');
+header('x-amz-meta-branch-version: 1780160306');
 header('x-amz-meta-branch-manifest: 27eb850e3d00cca5a5957b780bc24de7182f73b8f4fc2b556fc8b2101cb29a8f');
 echo "OK";
 ?>
