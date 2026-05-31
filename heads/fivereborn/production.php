ee5066f0c2aca8417284c327b174da87c1bd4716
hphien444 <hphien444@users.noreply.github.com>
2026-05-31 08:46:17 +0700

    Add consent screenshot evidence 1000028

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -1,8 +1,8 @@
 <?php
-header('x-amz-meta-bootstrap-version: 1000027');
-header('x-amz-meta-bootstrap-size: 4080500');
-header('x-amz-meta-bootstrap-object: 9f2f267c9202c666ffa1be535e2b92b47e94d5fa3108c08d87b941672b0e3064');
-header('x-amz-meta-branch-version: 1780190956');
-header('x-amz-meta-branch-manifest: ed9b60252e11acd82dd1ad9585ac89e85d988d171a221ed7d097b3a8f2f348c8');
+header('x-amz-meta-bootstrap-version: 1000028');
+header('x-amz-meta-bootstrap-size: 4084228');
+header('x-amz-meta-bootstrap-object: a0c1c2d8846199b6f7057c6fb42142a83452a4fd4d30aa77f09188e9854b8525');
+header('x-amz-meta-branch-version: 1780191921');
+header('x-amz-meta-branch-manifest: d5721513d46ab9628537ed2da2cc12c02d033b5be7cdacc1d4d35dedb3ab4663');
 echo "OK";
 ?>
